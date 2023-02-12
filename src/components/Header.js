5c28d7ad810a24519d129e4faa08ac2e55e39f91
Yehia-Gamal <yahia.elspaa@gmail.com>
2023-02-12 14:28:09 +0200

    Configure React app for deployment to GitHub Pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,10 +22,6 @@ const Header = () => {
                 <NavLink className="nav-link btn btn-outline-success" style={{ lineHeight: 1 }} to={"/"}>Add</NavLink>
               </li>
             </ul>
-            {/* <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form> */}
           </div>
         </div>
       </nav>
