663e7ab1599d7a2fd0dc9bba7416327f24d14a02
Yehia-Gamal <yahia.elspaa@gmail.com>
2023-02-13 18:27:08 +0200

    add Some edits general fixes

--- src/components/MovieControls.js
+++ src/components/MovieControls.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useMoviesContext } from './context/GlobalContext';
 import * as actions from "./context/ActionTypes"
 
--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useMoviesContext } from './context/GlobalContext';
 import "./ResultCard.css"
 import * as actions from "./context/ActionTypes"
