3376a2ae4104d18de4cf49864c0366cee2b14471
Yehia-Gamal <yahia.elspaa@gmail.com>
2023-02-14 22:23:37 +0200

    Convert formats style sheets to Bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,20 +5,20 @@ const Header = () => {
     <>
       <nav className="navbar navbar-expand-lg w-100">
         <div className="container">
-          <Link className="navbar-brand" to={"/"}>Movies</Link>
+          <Link className="navbar-brand fw-bold" to={"/"}>Movies</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
               <li className="nav-item">
-                <NavLink className="nav-link" to={"/watchlist"}>Watch List</NavLink>
+                <NavLink className="nav-link fw-bold" to={"/watchlist"}>Watch List</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to={"/watched"}>Watched</NavLink>
+                <NavLink className="nav-link fw-bold" to={"/watched"}>Watched</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link btn btn-outline-success" style={{ lineHeight: 1 }} to={"/"}>Add</NavLink>
+                <NavLink className="nav-link fw-bold btn btn-outline-success" style={{ lineHeight: 1 }} to={"/"}>Add</NavLink>
               </li>
             </ul>
           </div>
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,9 +4,9 @@ import MovieControls from './MovieControls';
 
 const MovieCard = ({ movie, type }) => {
   return (
-    <div className='movie-card'>
+    <div className='movie-card w-100 position-relative border-0 d-block'>
       <div className="overlay"></div>
-      {movie.Poster ? (<img src={movie.Poster} alt={movie.Title} />) : (<div className='filter-poster'></div>)}
+      {movie.Poster ? (<img className='w-100 h-100' src={movie.Poster} alt={movie.Title} />) : (<div className='filter-poster'></div>)}
       <MovieControls movie={movie} type={type}/>
     </div>
   );
